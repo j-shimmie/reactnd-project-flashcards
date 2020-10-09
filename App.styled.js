f1fcd4993f33f07f574aa4b5f0b95115caf1c61a
Jannet Shim <jshim@ellation.com>
2020-10-09 14:19:42 -0700

    Style with emotion

---
+++ App.styled.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/native'
+
+const S = {}
+
+S.App = styled.View`
+  flex: 1;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+`
+
+export default S
