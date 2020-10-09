4486c103563e2076af01146b0e92fd9f91a70f85
Jannet Shim <jshim@ellation.com>
2020-10-09 14:44:56 -0700

    Set up helper colors

--- App.styled.js
+++ App.styled.js
@@ -1,10 +1,12 @@
 import styled from '@emotion/native'
 
+import { gray } from './utils/colors'
+
 const S = {}
 
 S.App = styled.View`
   flex: 1;
-  background-color: white;
+  background-color: ${gray};
   align-items: center;
   justify-content: center;
 `
