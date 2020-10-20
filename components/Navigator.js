113c3842193f4c45ae8accfef532b903cd46c26f
Jannet Shim <jshim@ellation.com>
2020-10-19 18:25:34 -0700

    Add icons to tab navigation

--- components/Navigator.js
+++ components/Navigator.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import Decks from './decks/Decks'
 import DeckDetail from './deck-detail/DeckDetail'
@@ -8,6 +9,7 @@ import AddCard from './add-card/AddCard'
 import AddDeck from './add-deck/AddDeck'
 import Quiz from './quiz/Quiz'
 import QuizScore from './quiz-score/QuizScore'
+import { softBlue } from '../utils/colors'
 
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
@@ -16,7 +18,24 @@ const Tabs = () => {
   const { Navigator, Screen } = Tab
 
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName
+
+          if (route.name === 'Decks') {
+            iconName = 'ios-bookmarks'
+          } else if (route.name === 'Add Deck') {
+            iconName = 'ios-add-circle'
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: softBlue,
+      }}
+    >
       <Screen name="Decks" component={Decks} />
       <Screen name="Add Deck" component={AddDeck} />
     </Navigator>
